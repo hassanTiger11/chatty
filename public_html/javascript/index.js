809dc587f785be1057a204aae293348593d74ff8
hassanTiger11 <halnamer@email.arizona.edu>
2020-07-18 01:30:22 +0300

    files

---
+++ public_html/javascript/index.js
@@ -0,0 +1,71 @@
+/*
+Author: Hassan Alnamer
+This is the cliet side script for Chatty.
+The client side has two main functions:
+1) sendMessage() : send the message using AJAX and clear the textFields
+2) fetchMessage(): fetch messages from the server every 1 second. Does so, by setting the setInterval()
+The message should be shown upon recival fromt he server, not upon sending to the server
+*/
+
+/**
+This function uses the AJAX model to compose an http request and communicate with the server.
+The request I am going to use is GET since I am more fimiliar with it
+*/
+function sendMessage(){
+
+  var httpRequest = new XMLHttpRequest();
+
+  if(!httpRequest){
+    alert("Error!1");
+    return false;
+  }
+
+  httpRequest.onreadystatechange  = ()=>{
+    if(httpRequest.readyState === XMLHttpRequest.DONE){
+
+      if(httpRequest.status === 200){
+
+      }else{
+        document.getElementById('messageDisplay').value ="Failed!"
+      }
+    }
+  }
+
+  let user = document.getElementById('alias').value;
+  let message = document.getElementById('message').value;
+
+  let url = "/"+user+"/"+message;
+  console.log(url);
+  httpRequest.open('GET', url);
+  httpRequest.send();
+}
+
+function fetchMessages(){
+
+    var httpRequest = new XMLHttpRequest();
+
+    if(!httpRequest){
+      alert("Error!1");
+      return false;
+    }
+
+    httpRequest.onreadystatechange  = ()=>{
+      
+      if(httpRequest.readyState === XMLHttpRequest.DONE){
+
+        if(httpRequest.status === 200){
+
+          document.getElementById('chatDisplay').innerHTML  = httpRequest.responseText;
+
+        }else{
+          alert("couldn't refresh chat!");
+        }
+      }
+    }
+
+    let url = "/fetch";
+    console.log(url);
+    httpRequest.open('GET', url);
+    httpRequest.setHe
+    httpRequest.send();
+}
---
+++ server.js
@@ -0,0 +1,64 @@
+/**
+Author: Hassan Alnamer
+
+This is the server I am going to use for the chat
+The server runs a Express app and is tied with a mongoDB.
+I am going to use the mongoose module to access information from the DB and post to it.
+*/
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const mongoose = require("mongoose");
+var mongoDB = 'mongodb://127.0.0.1/chat';
+console.log("mongo: creating DB...");
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+console.log("mongo: DB created");
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+Schema = mongoose.Schema;
+ChatMessageSchema = new Schema({
+  time: Number,
+  alias: String,
+  message: String
+});
+ChatMessage = mongoose.model('ChatMessage', ChatMessageSchema );
+
+app.use(express.static('public_html'));
+
+app.get("/:user/:message", function(req, res){
+
+  console.log("message request...");
+  var userName = req.params.user;
+  console.log(req.params.user);
+  var text = req.params.message;
+  var date = new Date();
+  var currTime = date.getTime();
+  console.log("message request: collected params");
+  var message = new ChatMessage({
+    time: currTime,
+    alias: userName,
+    message: text});
+  message.save(function(err){
+    if(err) console.log("error with storing message");
+  });
+});
+
+//fetchMessage
+app.get("/fetch", function(req, res){
+  var messages = mongoose.model("ChatMessage", ChatMessageSchema);
+  var chat = "";
+  messages.find({}).exec(function(err, result){
+    for (i in result){
+      console.log( result[i].alias + ": "+result[i].message+"\n");
+      chat+= result[i].alias + ": "+result[i].message+"\n";
+    }
+  });
+  res.send(chat);
+  console.log(chat);
+
+});
+app.listen(port, function(){
+  console.log("listning on : http://$localhost:$port");
+});
